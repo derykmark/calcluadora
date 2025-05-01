1ca4ebb604223de243d89f5980baf2f245ff314a
Deryk Mark Kadiama <derykmark@hotmail.com>
2025-04-30 22:45:12 -0300

    Refatorando minha program

--- Program.cs
+++ Program.cs
@@ -1,70 +1,77 @@
 ﻿using DotNet.Operadores;
 
-decimal valor1 = 0, valor2 = 0, resultado, valorInformado1, valorInformado2, opcaoCalculadora;
-string operador, valorDigitado, valorDigitado2;
-bool isNumero1, isNumero2;
-
 Console.WriteLine("--------------- BEM VINDO A CALCULADORA --------------- \n");
-MontaCabecalhoCalculadora();
 
-operador = Console.ReadLine();
-opcaoCalculadora = int.Parse(operador);
 Operacoes operacoes = new Operacoes();
-var opcao = 0;
-while (opcao > -1)
+int opcao;
+
+do
 {
+    MontaCabecalhoCalculadora();
+    opcao = LerOpcaoUsuario();
+
+    if (opcao > 0 && opcao <= 4)
+    {
+        ExecutarOperacao(opcao, operacoes);
+    }
+} while (opcao != -1);
+
+static void ExecutarOperacao(int opcaoCalculadora, Operacoes operacoes)
+{
+    decimal valor1 = 0, valor2 = 0;
+    string valorDigitado, valorDigitado2;
+    bool isNumero1, isNumero2;
+
     switch (opcaoCalculadora)
     {
         case 1:
-            Console.WriteLine($"Opcao Selecionada {opcaoCalculadora}\n");
             Console.WriteLine("------------------- SOMA ------------------- \n");
-            MontaRequisicao(ref valor1, ref valor2, out valorDigitado, out valorDigitado2, out isNumero1, out isNumero2);
-            Console.WriteLine("RESULTADO");
-            operacoes.Somar(valor1, valor2);
-            Console.WriteLine("Informe (-1) para finalizar a calculadora \n");
-            MontaCabecalhoCalculadora();
-            opcao = int.Parse(Console.ReadLine());
-            opcaoCalculadora = opcao;
             break;
         case 2:
-            Console.WriteLine("\n");
-            Console.WriteLine("------------------- SUBTRACAO -------------------\n");
-            MontaRequisicao(ref valor1, ref valor2, out valorDigitado, out valorDigitado2, out isNumero1, out isNumero2);
-            Console.WriteLine("RESULTADO");
-            operacoes.Subtrair(valor1, valor2);
-            Console.WriteLine("Informe (-1) para finalizar a calculadora \n");
-            MontaCabecalhoCalculadora();
-            opcao = int.Parse(Console.ReadLine());
-            opcaoCalculadora = opcao;
+            Console.WriteLine("------------------- SUBTRACAO ------------------- \n");
             break;
         case 3:
-            Console.WriteLine("\n");
-            Console.WriteLine("------------------- MULTIPLICACAO -------------------\n");
-            MontaRequisicao(ref valor1, ref valor2, out valorDigitado, out valorDigitado2, out isNumero1, out isNumero2);
-            Console.WriteLine("RESULTADO");
-            operacoes.Multiplicar(valor1, valor2);
-            MontaCabecalhoCalculadora();
-            Console.WriteLine("Informe (-1) para finalizar a calculadora \n");
-            opcao = int.Parse(Console.ReadLine());
-            opcaoCalculadora = opcao;
+            Console.WriteLine("------------------- MULTIPLICACAO ------------------- \n");
             break;
         case 4:
-            Console.WriteLine("\n");
-            Console.WriteLine("------------------- DIVISAO -------------------\n");
-            MontaRequisicao(ref valor1, ref valor2, out valorDigitado, out valorDigitado2, out isNumero1, out isNumero2);
-            Console.WriteLine("RESULTADO");
-            operacoes.Dividir(valor1, valor2);
-            Console.WriteLine("Informe (-1) para finalizar a calculadora \n");
-            MontaCabecalhoCalculadora();
-            opcao = int.Parse(Console.ReadLine());
-            opcaoCalculadora = opcao;
+            Console.WriteLine("------------------- DIVISAO ------------------- \n");
             break;
         default:
-            Console.WriteLine("\n");
-            Console.WriteLine("Escolha uma Opcao Valida Entre 1 e 4 \n");
-            opcao = int.Parse(Console.ReadLine());
-            opcaoCalculadora = opcao;
-            break;
+            Console.WriteLine("Opcao invalida. Escolha entre 1 e 4.\n");
+            return;
+    }
+
+    MontaRequisicao(ref valor1, ref valor2, out valorDigitado, out valorDigitado2, out isNumero1, out isNumero2);
+
+    if (isNumero1 && isNumero2)
+    {
+        Console.WriteLine("RESULTADO");
+        switch (opcaoCalculadora)
+        {
+            case 1: operacoes.Somar(valor1, valor2); break;
+            case 2: operacoes.Subtrair(valor1, valor2); break;
+            case 3: operacoes.Multiplicar(valor1, valor2); break;
+            case 4: operacoes.Dividir(valor1, valor2); break;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Operacao nao realizada. Certifique-se de informar numeros validos.\n");
+    }
+}
+
+static int LerOpcaoUsuario()
+{
+    int opcao;
+    while (true)
+    {
+        Console.WriteLine("Informe uma opcao (1-4) ou (-1 para sair):");
+        string entrada = Console.ReadLine();
+        if (int.TryParse(entrada, out opcao))
+        {
+            return opcao;
+        }
+        Console.WriteLine("Entrada invalida. Tente novamente.\n");
     }
 }
 
@@ -75,9 +82,13 @@ static void MontaRequisicao(ref decimal valor1, ref decimal valor2, out string v
     isNumero1 = VerificaSeENumero(valorDigitado);
 
     if (isNumero1)
-    { valor1 = decimal.Parse(valorDigitado); }
+    {
+        valor1 = decimal.Parse(valorDigitado);
+    }
     else
-        Console.WriteLine("Informe um numero");
+    {
+        Console.WriteLine("Informe um numero valido.");
+    }
 
     Console.WriteLine("Informe o Segundo Numero:");
     valorDigitado2 = Console.ReadLine();
@@ -85,32 +96,25 @@ static void MontaRequisicao(ref decimal valor1, ref decimal valor2, out string v
     Console.WriteLine("\n");
 
     if (isNumero2)
-    { valor2 = decimal.Parse(valorDigitado2); }
+    {
+        valor2 = decimal.Parse(valorDigitado2);
+    }
     else
-        Console.WriteLine("Informe um numero");
+    {
+        Console.WriteLine("Informe um numero valido.");
+    }
 }
+
 static bool VerificaSeENumero(string numero)
 {
-    bool verifica;
-    decimal valor;
-    verifica = decimal.TryParse(numero, out valor);
-    return verifica;
+    return decimal.TryParse(numero, out _);
 }
 
 static void MontaCabecalhoCalculadora()
 {
-    
     Console.WriteLine("Selecione uma opcao para realizar o Calculo: \n");
     Console.WriteLine("1 - Soma");
     Console.WriteLine("2 - Subtracao");
     Console.WriteLine("3 - Multiplicacao");
     Console.WriteLine("4 - Divisao \n");
 }
-
-
-
-//Operacoes operacoes = new Operacoes();
-//operacoes.Somar(20, 10);
-//operacoes.Subtrair(20, 10);
-//operacoes.Multiplicar(20, 10);
-//operacoes.Dividir(20, 10);
